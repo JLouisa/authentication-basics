85b343241f37bda4c9a16f6eb6096d1d7818f8f5
Jlouisa <jonathan.louisa@gmail.com>
2023-10-31 16:30:47 +0100

    Add bcryptjs for password hashing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
+const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
@@ -35,7 +36,9 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
         return done(null, false, { message: "Incorrect password" });
       }
       return done(null, user);
@@ -70,16 +73,18 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => res.render("index", { user: res.locals.currentUser }));
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post("/sign-up", async (req, res, next) => {
-  try {
-    const user = new User({
-      username: req.body.username,
-      password: req.body.password,
-    });
-    const result = await user.save();
-    res.redirect("/");
-  } catch (err) {
-    return next(err);
-  }
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    try {
+      const user = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      const result = await user.save();
+      res.redirect("/");
+    } catch (err) {
+      return next(err);
+    }
+  });
 });
 app.post(
   "/log-in",
